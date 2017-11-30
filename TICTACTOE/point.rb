d8ff24e53a1590ee539b558e856e921c47c3fea3
nejcik <nataliaxskowronek@gmail.com>
2017-11-30 19:30:01 +0100

    Change point access

--- TICTACTOE/point.rb
+++ TICTACTOE/point.rb
@@ -1,11 +1,11 @@
-class Point < Board
+class Point 
 	attr_accessor :original_value, :x, :y, :symbol, :weight
 
 
 	def initialize(value,players_symbol)
 		@original_value=value
 		@symbol = players_symbol
-		set_index(original_value)
+		set_indexes(original_value)
 		set_weight(symbol)
 	end
 
@@ -16,7 +16,7 @@ class Point < Board
 	end
 
 	def get_extented_index(index)
-		point_value=2*index-1
+		point_value=2*index+1
 	end
 
 	def set_weight(symbol)
--- TICTACTOE/point_test.rb
+++ TICTACTOE/point_test.rb
@@ -17,28 +17,22 @@ class PointTest < Minitest::Test
 
 	def test_change_to_x
 		point=Point.new("A1","O")
-		assert_equal 1, point.x
+		assert_equal 0, point.x
 	end
 
 	def test_change_to_y
 		point=Point.new("A1","O")
-		assert_equal 1, point.y
+		assert_equal 0, point.y
 	end
 
-	def test_change_to_y
+	def test_change_EXT_to_y
 		point=Point.new("B3","X")
-		assert_equal 5, point.y
+		assert_equal 5, point.get_extented_index(point.y)
 	end
 
-
-	def test_not_change_to_y
-		point=Point.new("C4","O")
-		refute_equal 4, point.y
-	end
-
-	def test_not_change_to_y
-		point=Point.new("B3","O")
-		refute_equal 2, point.y
+	def test_change_EXT_to_x
+		point=Point.new("B3","X")
+		assert_equal 3, point.get_extented_index(point.x)
 	end
 
 	def test_set_proper_weight
