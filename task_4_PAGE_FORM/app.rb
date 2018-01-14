ae262d8b85c190549d7827988ca9f33d5950b829
nejcik <nataliaxskowronek@gmail.com>
2018-01-14 14:35:16 +0100

    Add config.ru

--- task_4_PAGE_FORM/app.rb
+++ task_4_PAGE_FORM/app.rb
@@ -5,36 +5,39 @@ Bundler.require(:default)
 require 'sinatra/reloader'
 require_relative 'simple_user'
 
-user = SimpleUser.new
 
-get '/' do
-  if user.current_user 
-    redirect '/logged' 
-  else 
+class App < Sinatra::Base
+  user = SimpleUser.new
+
+  get '/' do
+    if user.current_user 
+      redirect '/logged' 
+    else 
+      erb :index
+    end
+  end
+
+  post '/' do
+    user.current_user = false
+
     erb :index
   end
-end
 
-post '/' do
-  user.current_user = false
-
-  erb :index
-end
-
-get '/logged' do
-  if user.current_user 
-    erb :logged
-  else 
-    redirect '/'
+  get '/logged' do
+    if user.current_user 
+      erb :logged
+    else 
+      redirect '/'
+    end
   end
-end
 
-post '/logged' do
-  user.input_password = params[:password]
-  if user.check_password(user.input_password)
-    user.current_user = true
-    erb :logged
-  else
-    redirect '/'
+  post '/logged' do
+    user.input_password = params[:password]
+    if user.check_password(user.input_password)
+      user.current_user = true
+      erb :logged
+    else
+      redirect '/'
+    end
   end
 end
